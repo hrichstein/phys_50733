bba91a8a232acea52120e02c19b3ba6a3ffaaf92
Hannah Richstein <hannah.richstein@live.com>
2017-02-07 09:22:52 -0600

    File with heading information needed for all class assignments.

---
+++ hw_heading_file.py
@@ -0,0 +1,29 @@
+"""
+
+Student Name: Hannah Richstein
+Professor Name: Dr. Frinchaboy
+Class: PHYS 50733
+ASSIGNMENT
+Last edited: 
+
+Overview:
+---------
+
+
+Input:
+------
+
+
+Output:
+-------
+
+
+Program Limitations:
+--------------------
+
+
+Significant Program Variables:
+------------------------------
+
+
+"""
