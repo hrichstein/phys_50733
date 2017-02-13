38e8553815b09ca584607ea79dae000e53b03d73
Hannah Richstein <hannah.richstein@live.com>
2017-02-12 20:45:54 -0600

    Just committing before I delete lines

--- rh_hw3/richstein_hw3_prob2a.py
+++ rh_hw3/richstein_hw3_prob2a.py
@@ -35,7 +35,7 @@ c = float(input("c: "))
 
 
 def quad_func(a, b, c):
-	discriminant = sqrt(b**2 - (4*a*c))
+	discriminant = sqrt(b**2 - (4 * a * c))
 
 	x_pos_d = discriminant
 	x_neg_d = -discriminant
@@ -43,11 +43,28 @@ def quad_func(a, b, c):
 	results = array([x_pos_d, x_neg_d])
 
 	results += (-b)
-	results /= (2*a)
+	results /= (2 * a)
 
 	return sort(results)
 
 
 results_test = array(quad_func(a, b, c))
 
-print("\nSolutions: ({0}, {1})".format(results_test[0], results_test[1]))
+print("\nSolutions, Method 1: ({0}, {1})".format(results_test[0],
+      results_test[1]))
+
+
+def sec_q_func(a, b, c):
+	numerator = 2 * c
+	discriminant = sqrt(b**2 - (4 * a * c))
+
+	plus_denom = -b + discriminant
+	neg_denom = -b - discriminant
+
+	results = array([(numerator / plus_denom), (numerator / neg_denom)])
+
+	return sort(results)
+
+func_2_res = sec_q_func(a, b, c)
+
+print("\nSolutions, Method 2: ({0}, {1})".format(func_2_res[0], func_2_res[1]))
--- rh_hw3/richstein_hw3_prob2c.py
+++ rh_hw3/richstein_hw3_prob2c.py
@@ -29,3 +29,52 @@ Significant Program Variables:
 """
 from numpy import sqrt, array, sort
 
+a = float(input("Please enter the three coefficients. \na: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
+
+def new_quad(a, b, c):
+	disc_1 = b**2
+	disc_2 = 4 * a * c
+	disc_3 = disc_1 - disc_2
+	disc_4 = sqrt(disc_3)
+
+	# first_term = -b / (2 * a)
+
+	# sec_term = disc_4 / (2 * a)
+
+	# results = array([(first_term + sec_term), (first_term - sec_term)])
+
+	# return sort(results)
+
+	# pos_d_num = -b + disc_4
+	# neg_d_num = -b - disc_4
+
+	# denom = 2 * a
+
+	# results = array([(pos_d_num / denom), (neg_d_num / denom)])
+
+	# return sort(results)
+
+	# num = 2 * c
+	# pos_d_num =
+
+	# first_term = -b / (2 * c)
+
+	# sec_term = disc_4 / (2 * c)
+
+	# results = array([1 / (first_term + sec_term), 1 / (first_term - sec_term)])
+
+	large_root = (-b - disc_4) / (2 * a)
+	small_root = (c / a) / large_root
+
+	results = array([large_root, small_root])
+
+	return sort(results)
+
+
+results_test = array(new_quad(a, b, c))
+
+print("\nSolutions, Method 3: ({0}, {1})".format(results_test[0],
+      results_test[1]))
