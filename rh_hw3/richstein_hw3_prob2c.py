4b373633ace20d80b1f27186213ebe2429337b15
Hannah Richstein <hannah.richstein@live.com>
2017-02-12 18:37:34 -0600

    Renamed stuff

---
+++ rh_hw3/richstein_hw3_prob2c.py
@@ -0,0 +1,31 @@
+"""
+
+Student Name: Hannah Richstein
+Professor Name: Dr. Frinchaboy
+Class: PHYS 50733
+HW 3, Prob 2c
+Last edited: 12 February 2017
+
+Overview:
+---------
+
+
+Input:
+------
+
+
+Output:
+-------
+
+
+Program Limitations:
+--------------------
+
+
+Significant Program Variables:
+------------------------------
+
+
+"""
+from numpy import sqrt, array, sort
+
