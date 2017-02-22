063671a0c3b5c049550bc1c6e423374e5d27b569
Hannah Richstein <hannah.richstein@live.com>
2017-02-21 22:31:38 -0600

    Adding primary stuff for hw4

---
+++ rh_hw4/richstein_hw4_prob1.py
@@ -0,0 +1,21 @@
+"""
+
+Student Name: Hannah Richstein
+Professor Name: Dr. Frinchaboy
+Class: PHYS 50733
+ASSIGNMENT
+Last edited:
+
+Overview:
+---------
+
+
+Input:
+------
+
+
+Output:
+-------
+
+
+"""
