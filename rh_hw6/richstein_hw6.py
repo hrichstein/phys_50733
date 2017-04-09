473f87f3faa013d1b1d3ade65a632203f879a62c
Hannah Richstein <hannah.richstein@live.com>
2017-04-09 00:05:13 -0500

    semi working

--- rh_hw6/richstein_hw6.py
+++ rh_hw6/richstein_hw6.py
@@ -42,38 +42,73 @@ h = 0.5 # step size
 
 # define some function here
 
+r = [theta_init, 0]
+
 def pend_func(r,t):
-	theta = r[0]
+	# print(r)
+	theta = r[0]  # in degrees
 	omega = r[1]
 	d_theta = omega
-	d_omega = -(g/l)*np.sin(theta*np.pi/180)
+	d_omega = -(g/arm_length)*np.sin(theta*np.pi/180)
+
 	return np.array([d_theta,d_omega],float)
+
 
 a = 0.0  # first time step
 b = 10.0  # last time step
-N = 10  # Number of steps
-h = (b-a)/N  # step size
+# N = 10  # Number of steps
+# h = (b-a)/N  # step size
 
 tpoints = np.arange(a, b, h)
-xpoints = []
-x = 0.0
+xpoints = []  # actualy theta values
+x = r
 
 for t in tpoints:
-	xpoints.append(x)
+	xpoints.append(x[0])  # theta values
 	k1 = h * pend_func(x,t)
 	k2 = h * pend_func(x + 0.5*k1, t + 0.5*h)
 	k3 = h * pend_func(x + 0.5*k2, t + 0.5*h)
 	k4 = h * pend_func(x + k3, t + h)
 	x += (k1+2*k2+2*k3+k4)/6
+	# print(x)
 
 # b)
 
 # Leapfrog
 
 theta_2_init = -92 # degrees 
-x2 = 0.0
+x2points = []
 
-f1 = x2 + 0.5*h*pend_func
+r2 = [theta_2_init, 0]
+x2 = r2
+
+def leapfrog(init, half, t, array):
+	next_full = init + h * pend_func(half, t + 0.5*h)
+	array.append(next_full[0])
+	next_half = half + h * pend_func(next_full, t + h)
+	array.append(next_half[0])
+
+	return np.array([next_full,next_half])
+
+
+x2points.append(x2[0])
+f0 = r2[0]
+f1 = x2 + 0.5*h * pend_func(x2, t)  # x(t+0.5h)
+
+input_arr = [f0,f1]
+
+for t in tpoints:
+	input_arr = leapfrog(input_arr[0], input_arr[1], t, x2points)
+
+
+
+# f1 = x2 + 0.5*h * pend_func(x2, t)  # x(t+0.5h)
+
+
+
+# f2 = x2 + h * pend_func(f1, t + 0.5*h)  # x(t+h)
+# f3 = f1 + h * pend_func(f2, t + h)  # x(t+1.5h)
+# f4 = f2 + h * pend_func(f3, t + h)  # x(t+2h)
 
 
 
