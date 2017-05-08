50f754f2de08d2909b015b9f656f60249ff2040f
Hannah Richstein <hannah.richstein@live.com>
2017-05-07 21:00:50 -0500

    Updating

--- rh_project/seventh_heaven.py
+++ rh_project/seventh_heaven.py
@@ -65,9 +65,9 @@ def find_vel_init(Mc, a):
 	# print(2*np.pi*a/v) Check for whether the circumference divided by the
 	# velocity actually gave me the period found above
 
-	return v
+	return period, v
 
-def ghetto2(param):
+def rk4(param):
 
 	"""
 	This function takes in an array containing the positions and velocities of 
@@ -85,6 +85,8 @@ def ghetto2(param):
 
 	Output:
 	------
+	An array of the same format as what was input, except now with the
+	derivatives of each quantity
 
 	"""
 
@@ -117,41 +119,46 @@ def ghetto2(param):
 
 	return np.array([vs1, vs2, vp, a1, a2, ap])
 
+
+# Defining constants
 G = 4 * np.pi**2  # AU^3 yr^-2 M_sun^-1
 
+# The separation between the binary stars
 A = 20  # AU
-r = A/2  # semi-major axis & radius
-test_plan = 50 # AU
+r = A/2  # semi-major axis & radius of circle they will be orbiting
 
-a = 0
-b = 420
-N = 1e6
-h = (b-a)/N
+# Testing radius of planet
+test_plan = 200  # AU
 
+# Mass Settings
 M1 = 1
 M2 = 1
 
-red_mass = M1*M2/(M1+M2)
+red_mass = M1*M2/(M1+M2)  # Reduced mass
+
+# Calculating initial velocities
+s_period, s_vel = find_vel_init(red_mass, r)
+p_period, p_vel = find_vel_init(red_mass, test_plan)
+
+# Time setup
+a = 0
+b = p_period
+N = 5e6
+h = (b-a)/N
 
 tpoints = np.arange(a, b, h, dtype=int)	
 
+# Initial conditions
+	# Positions
 s1 = np.array([r, 0], float)
 s2 = np.array([-r,0], float)
 p = np.array([test_plan, 0], float)
-
-s_vel = find_vel_init(red_mass, r)
-
-p_vel = find_vel_init(red_mass, test_plan)
-
-# s_vel = np.sqrt(10*G*red_mass)
-
-# print("S-vel : {0:.2f}".format(s_vel))
-
+	# Velocities
 s1_v0 = np.array([0, s_vel], float)
 s2_v0 = np.array([0, -s_vel], float)
 p_v0 = np.array([0, p_vel], float)
 
-
+# Creating empty arrays to be filled during the Runge-Kutta process
 xpts_s1 = [[] for tt in range(len(tpoints))]
 ypts_s1 = [[] for tt in range(len(tpoints))]
 
@@ -161,44 +168,63 @@ ypts_s2 = [[] for tt in range(len(tpoints))]
 xpts_p = [[] for tt in range(len(tpoints))]
 ypts_p = [[] for tt in range(len(tpoints))]
 
-s_ghet = np.array([s1, s2, p, s1_v0, s2_v0, p_v0])
+# Array to be passed to Runge-Kutta
+rk_params = np.array([s1, s2, p, s1_v0, s2_v0, p_v0])
+# Flag-like variable to tell me if the Runge-Kutta stopped due to the planet
+# exceeding three times its escape velocity (Why keep running it after you
+# know it's being flung out)
+final_time = 0
 
+# Runge-Kutta implementation
 for tt in range(len(tpoints)):
-	xpts_s1[tt] = s_ghet[0][0]
-	ypts_s1[tt] = s_ghet[0][1]
+	# Saving x and y points for plotting
+	xpts_s1[tt] = rk_params[0][0]
+	ypts_s1[tt] = rk_params[0][1]
 
-	xpts_s2[tt] = s_ghet[1][0]
-	ypts_s2[tt] = s_ghet[1][1]
+	xpts_s2[tt] = rk_params[1][0]
+	ypts_s2[tt] = rk_params[1][1]
 
-	xpts_p[tt] = s_ghet[2][0]
-	ypts_p[tt] = s_ghet[2][1]
+	xpts_p[tt] = rk_params[2][0]
+	ypts_p[tt] = rk_params[2][1]
 
-	k1 = h * ghetto2(s_ghet)
-	k2 = h * ghetto2(s_ghet + 0.5*k1)
-	k3 = h * ghetto2(s_ghet + 0.5*k2)
-	k4 = h * ghetto2(s_ghet + k3)
+	# Actual Runge-Kutta
+	k1 = h * rk4(rk_params)
+	k2 = h * rk4(rk_params + 0.5*k1)
+	k3 = h * rk4(rk_params + 0.5*k2)
+	k4 = h * rk4(rk_params + k3)
 
-	s_ghet += (k1 + 2*k2 + 2*k3 + k4) / 6
+	rk_params += (k1 + 2*k2 + 2*k3 + k4) / 6
 
-	vel = np.sqrt(s_ghet[-1][0]**2 + s_ghet[-1][1]**2)
-	rad = np.sqrt(s_ghet[2][0]**2 + s_ghet[2][1]**2)
-
+# 	# Checking to see if the planet has been flung off yet
+	vel = np.sqrt(rk_params[-1][0]**2 + rk_params[-1][1]**2)
+	rad = np.sqrt(rk_params[2][0]**2 + rk_params[2][1]**2)
 	esc_vel = np.sqrt(2 * G * red_mass / rad)
 
+	# If the planet has been, stop the loop
 	if vel >= (3*esc_vel):
 		final_time = tt
+		print("Velocity end")
 		break
 
-xpts_s1 = xpts_s1[:final_time]
-ypts_s1 = ypts_s1[:final_time]
+# If the planet has been flung out, cut the arrays short so that they can be
+# plotted.
+if final_time != 0:
+	xpts_s1 = xpts_s1[:final_time]
+	ypts_s1 = ypts_s1[:final_time]
 
-xpts_s2 = xpts_s2[:final_time]
-ypts_s2 = ypts_s2[:final_time]
+	xpts_s2 = xpts_s2[:final_time]
+	ypts_s2 = ypts_s2[:final_time]
 
-xpts_p = xpts_p[:final_time]
-ypts_p = ypts_p[:final_time]
+	xpts_p = xpts_p[:final_time]
+	ypts_p = ypts_p[:final_time]
 
-plt.plot(xpts_s1, ypts_s1)
-plt.plot(xpts_s2, ypts_s2)
-plt.plot(xpts_p, ypts_p)
-plt.show()	
+# Plotting
+fig, ax = plt.subplots()
+ax.set_xlabel("AU")
+ax.set_ylabel("AU")
+ax.set_title("Stars {0} AU Apart, Planet {1} AU Away".format(A, test_plan))
+ax.plot(xpts_s1, ypts_s1, label="Star 1")
+ax.plot(xpts_s2, ypts_s2, label="Star 2")
+ax.plot(xpts_p, ypts_p, label ="Planet")
+plt.legend(loc="best")
+plt.show()
